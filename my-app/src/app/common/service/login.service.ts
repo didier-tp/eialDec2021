1ded098733e2cf0dfe602aeb3117c352a5f1d564
didierDefrance <didier@d-defrance.fr>
2021-12-09 13:46:40 +0100

    s

--- my-app/src/app/common/service/login.service.ts
+++ my-app/src/app/common/service/login.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Login } from '../data/login';
 import { LoginResponse } from '../data/loginResponse';
 
@@ -14,8 +15,23 @@ export class LoginService {
 
   public postLogin$(login: Login): Observable<LoginResponse>{
     let url = this.baseUrl + "/public/auth"
-    return this._http.post<LoginResponse>(url,login);
-  }
+    return this._http.post<LoginResponse>(url,login).pipe(
+      tap((loginResponse)=>{ 
+              this.sauvegarderJeton(loginResponse);}
+         )
+  );
+}
+
+private sauvegarderJeton(loginResponse:LoginResponse){
+if(loginResponse.status){
+  if(loginResponse.token !=null)
+      sessionStorage.setItem('authToken',loginResponse.token);
+//ou autre façon de mémoriser le jeton
+}
+else{
+   sessionStorage.setItem('authToken',"");
+}
+}
 
   constructor(private _http : HttpClient) { }
 }
